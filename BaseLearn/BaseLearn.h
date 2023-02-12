3f987828343ac7ad08fc07f20fcf4cb2d4562274
mspoonam <mspoonam.learning@gmail.com>
2023-02-11 20:56:35 -0500

    Initial Commit

---
+++ BaseLearn/BaseLearn.h
@@ -0,0 +1,18 @@
+//
+//  BaseLearn.h
+//  BaseLearn
+//
+//  Created by Poonam Pandey on 11/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseLearn.
+FOUNDATION_EXPORT double BaseLearnVersionNumber;
+
+//! Project version string for BaseLearn.
+FOUNDATION_EXPORT const unsigned char BaseLearnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseLearn/PublicHeader.h>
+
+
